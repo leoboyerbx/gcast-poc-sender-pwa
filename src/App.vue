36615ec5976fd3ad8e8f930be864c86b6cbe62ee
leoboyerbx <contact@leoboyer.fr>
2021-04-14 20:26:29 +0200

    switch pwa finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <div class="cast-button">
+      <google-cast-launcher></google-cast-launcher>
+    </div>
+<!--    <QrcodeStream @decode="onDecode"></QrcodeStream>-->
+    <h1 v-if="state === 'notconnected'">En attente</h1>
+    <h1 v-if="state === 'connected'">Le jeu !</h1>
+  </main>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { v4 as uuid } from 'uuid'
+import { io } from 'socket.io-client'
+// import {QrcodeStream} from 'vue-qrcode-reader/src'
+
+const sessionId = uuid()
+const socket = io('https://cast.leoboyer.dev')
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // components: {QrcodeStream},
+  data() {
+    return {
+      state: 'notconnected'
+    }
+  },
+  created() {
+    window['__onGCastApiAvailable'] = (isAvailable) => {
+      console.log('init', isAvailable)
+      if (isAvailable) {
+        this.initializeCastApi();
+      }
+    };
+
+    // bindings
+    socket.on('ready', () => this.state = 'connected')
+  },
+  methods: {
+    onDecode(decodedString) {
+
+
+    },
+    initializeCastApi() {
+      cast.framework.CastContext.getInstance().setOptions({
+        receiverApplicationId: '720A8D7E',
+        autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+      })
+      cast.framework.CastContext.getInstance().addEventListener(cast.framework.CastContextEventType.SESSION_STATE_CHANGED, e => {
+        console.log(e)
+        if (e.sessionState === 'SESSION_STARTED' || e.sessionState === 'SESSION_RESUMED') {
+          this.pairWithCast()
+          this.pairWithServer()
+        }
+      })
+    },
+    pairWithCast() {
+      const castSession = cast.framework.CastContext.getInstance().getCurrentSession();
+      if(castSession){
+        castSession.sendMessage('urn:x-cast:com.pnk.poccast', {
+          type: "syncID",
+          text: sessionId
+        });
+      }
+    },
+    pairWithServer () {
+      console.log({
+        id: sessionId,
+        type: 'player'
+      })
+      socket.emit('config', {
+        id: sessionId,
+        type: 'player'
+      })
+    }
   }
 }
 </script>
@@ -24,5 +85,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.cast-button {
+  width: 30px;
+  height: 30px;
 }
 </style>
